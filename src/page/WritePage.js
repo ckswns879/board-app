7833aa5e772c575e03b595ffbe2a2651670b5f4c
chan jun <ckswns879@naver.com>
2023-06-23 15:34:15 +0900

    feat: 글쓰기

--- src/page/WritePage.js
+++ src/page/WritePage.js
@@ -2,9 +2,21 @@
 import React from 'react';
 
 const WritePage = () => {
+
+const handWrite = () => {
+//ListPage의 setPosts에 무엇을 담아야하는가
+let post = {id:6, title:"input값"};
+
+}
+
     return (
         <div>
             <h1>글쓰기 페이지</h1>
+            <hr />
+            <form>
+                <input type='text' placeholder='제목을 입력하세요...' />
+                <button type='button' onClick={handWrite}>글쓰기</button>
+            </form>
         </div>
     );
 };
