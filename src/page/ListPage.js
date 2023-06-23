4eb6b400279f0b4687564357af784670604e2aad
chan jun <ckswns879@naver.com>
2023-06-23 15:34:03 +0900

    feat: 글목록보기

--- src/page/ListPage.js
+++ src/page/ListPage.js
@@ -3,9 +3,13 @@ import styled from 'styled-components';
 
 // styled-components version6이후로 설치문제발생 npm install styled-components@5.3.10 설치
 const StyldeitemBoxDiv = styled.div`
-border:1px, solid black;
+display: flex;
+justify-content: space-between;
+border:2px solid black;
 padding: 10px;
 height: 100px;
+margin: 20px;
+align-items: center;
 `
 
 const ListPage = () => {
@@ -16,15 +20,20 @@ const [posts, setPosts] = useState([
     {id:3, title: "내용3"},
     {id:4, title: "내용4"},
     {id:5, title: "내용5"},
-])
+]);
+
+
 
     return (
         <div>
             <h1>리스트 페이지</h1>
             <hr />
             {posts.map((post) => (
-            <StyldeitemBoxDiv>
-                번호 : {post.id} 제목 : {post.title}
+            <StyldeitemBoxDiv key={post.id}>
+                <div>
+                번호:{post.id}  제목:{post.title}
+                </div>
+                <button>삭제</button>
                 </StyldeitemBoxDiv>
                 ))}
         </div>
