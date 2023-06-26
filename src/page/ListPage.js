f4a5f75f121c9c6ec8493bdbb85f3832cc6f265a
chan jun <ckswns879@naver.com>
2023-06-26 16:17:45 +0900

    feat: 글쓰기, 삭제하기 추가

--- src/page/ListPage.js
+++ src/page/ListPage.js
@@ -14,28 +14,57 @@ align-items: center;
 
 const ListPage = () => {
 
+const [no, setNo] = useState(6);
+
+const [post, setPost] = useState({
+id:no,
+title: "",
+content: ""
+});
+
 const [posts, setPosts] = useState([
-    {id:1, title: "내용1"},
-    {id:2, title: "내용2"},
-    {id:3, title: "내용3"},
-    {id:4, title: "내용4"},
-    {id:5, title: "내용5"},
+    {id:1, title: "제목1", content: "내용1"},
+    {id:2, title: "제목2", content: "내용2"},
+    {id:3, title: "제목3", content: "내용3"},
+    {id:4, title: "제목4", content: "내용4"},
+    {id:5, title: "제목5", content: "내용5"},
 ]);
 
+const handWrite = () => {
+    setPosts([...posts, { id: no, title: post.title, content: post.content }]);
+    setNo(no + 1);
+  };
 
+const handleForm = (e) => {
+    console.log(e.target.name);
+    console.log(e.target.value);
+
+    // computed property names 문법 (키값 동적할당)
+    setPost({...post, [e.target.name]: e.target.value}); // 변수를동적으로만듬 // ...post(기존값을두고 업데이트)
+};
+
+const handleDelete = (id) => {
+    const updatedPosts = posts.filter((post) => post.id !== id);
+    setPosts(updatedPosts);
+  };
 
     return (
         <div>
             <h1>리스트 페이지</h1>
+            <form>
+                <input type='text' placeholder='제목을 입력하세요...' value={post.title} onChange={handleForm} name='title'/>
+                <input type='text' placeholder='내용을 입력하세요...' value={post.content} onChange={handleForm} name='content'/>
+                <button type='button' onClick={handWrite}>글쓰기</button>
+            </form>
             <hr />
             {posts.map((post) => (
             <StyldeitemBoxDiv key={post.id}>
                 <div>
-                번호:{post.id}  제목:{post.title}
+                번호:{post.id} / 제목:{post.title} / 내용:{post.content}
                 </div>
-                <button>삭제</button>
-                </StyldeitemBoxDiv>
-                ))}
+                <button onClick={() => handleDelete(post.id)}>삭제</button>
+            </StyldeitemBoxDiv>
+            ))}
         </div>
     );
 };
