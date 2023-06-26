5e077c0a35b7e8e1afcb4ad72c29a0e8b6185c9f
chan jun <ckswns879@naver.com>
2023-06-26 16:17:01 +0900

    refactor : refactoring

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
-import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import Navigation from './components/Navigation';
 import ListPage from './page/ListPage';
-import WritePage from './page/WritePage';
 
 function App() {
   return (
 <>
-<Navigation />
-<Routes>
-<Route path='/' exact={true} element={<ListPage />} />
-<Route path='/write' exactct={true} element={<WritePage />} />
-</Routes>
+<ListPage />
 </>
   );
 }
